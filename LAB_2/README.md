6c3e03594a92170d0d2fc1d8438c3ce4325d33fe
majorlenox <majorlenox@mail.ru>
2022-03-03 20:03:37 +0300

    LAB2: added README.md, next step: add option to save graph.bin

---
+++ LAB_2/README.md
@@ -0,0 +1,12 @@
+LAB_2: Python 3.9, Creating random graphs and finding Hamiltonian cycles and Eulerian cycles  
+
+    graph_creator.py: 
+        - Creates random graph and save it in graph.bin
+        Using:
+            python3 graph_creator.py -V 15 -E 20 -S
+            flags: 
+                -V [number_of_vertexes] - how much vertexes will be in graph, by default = 10
+                -E [number_of_edges]    - how much edges will be in graph, by default = random from V - 1 to 3V 
+                -S                      - programm will save picture of graph in graph.png
+
+    
--- LAB_2/graph_creator.py
+++ LAB_2/graph_creator.py
@@ -1,39 +1,75 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 import random
 import sys
 
-def check_cycle(graph):
-    E = 0
-    for v in range(V):
-        while (graph[v+1].has(v+1)):
 
-            E += 1
-    return E
+# Visualisation
+
+def showGraph(graph, vertexes):
+    G = nx.Graph()
+    G.add_edges_from(graph)
+    color_map = ["red" for i in range(len(vertexes))]
+    nx.draw_networkx(G, with_labels=True, font_color='black', font_size=16, node_color=color_map)
+    plt.savefig("Graph", dpi=120)
+
+
+# Generation
+
+def kruskals_algorithm(graph, E, vertexes):
+    V = len(vertexes)
+    E -= V - 1
+    color = {}
+    for i in range(V):
+        color[vertexes[i]] = vertexes[i]
+
+    for e in range(V - 1):
+        while len(set(list(color.values()))) != 1:
+            v_from = vertexes[random.randint(0, V - 1)]
+            v_to = vertexes[random.randint(0, V - 1)]
+            if color[v_from] != color[v_to]:
+                for i in range(V - 1):
+                    if color[vertexes[i]] == color[v_to]:
+                        color[vertexes[i]] = color[v_from]
+                graph.append([v_from, v_to])
+
 
 if __name__ == '__main__':
 
     # init V,E
-    V = 100
-    E = random.randint(int(V - 1), V * 3)
+    V = 10
+    if sys.argv.__contains__('-V'):
+        V = int(sys.argv[sys.argv.index('-V') + 1])
+    E = random.randint(V - 1, V * 3)
 
-    if len(sys.argv) == 1:
-        V = int(sys.argv[1])
-        E = random.randint(int(V - 1), V * 3)
-
-    if len(sys.argv) == 2:
-        V = int(sys.argv[1])
-        E = int(sys.argv[2])
-        if E < V - 1:
+    if sys.argv.__contains__('-E'):
+        E = int(sys.argv[sys.argv.index('-E') + 1])
+        if (E < V - 1) | (V <= 0):
             print("It is impossible to create a connected graph")
             exit(1)
     #
 
-    graph = {}
+    graph = []
+    vertexes = []
+
+    # naming
+    name_from = 100
+    name_to = name_from * 10 - 1
+    while V >= (name_from * 10 - name_from):
+        name_from *= 10
+        name_to = name_from * 10 - 1
 
     for v in range(V):
-        graph[v+1] = []
+        vertexes.append(random.randint(name_from, name_to))
 
-    for e in range(E):
-        graph[random.randint(1, V)].append(random.randint(1, V))
+    kraskals_algorithm(graph, E, vertexes)  # only linked graphs
 
-    while (cycles = check_cycle(graph)):
-
+    for e in range(E - (V - 1)):
+        v_from = vertexes[random.randint(0, V - 1)]
+        v_to = vertexes[random.randint(0, V - 1)]
+        while v_to == v_from:  # no zero cycles
+            v_to = vertexes[random.randint(0, V - 1)]
+        graph.append([v_from, v_to])
+    if sys.argv.__contains__("-S"):
+        showGraph(graph, vertexes)
